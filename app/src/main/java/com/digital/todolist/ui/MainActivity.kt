43f0f50f3c66d2e5e9d68c9a0edb65fafeac2828
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2021-08-18 12:02:07 -0400

    Finalizando o app

--- app/src/main/java/com/digital/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/digital/todolist/ui/MainActivity.kt
@@ -2,9 +2,7 @@ package com.digital.todolist.ui
 
 import android.app.Activity
 import android.content.Intent
-import android.opengl.Visibility
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.digital.todolist.databinding.ActivityMainBinding
